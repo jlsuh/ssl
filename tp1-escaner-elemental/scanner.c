e3f883118d47d2bdb788d0af7de5b0e589334195
Joel <joelleonardosuh@gmail.com>
2020-08-30 15:51:56 -0300

    Subiendo versión con reducción de estados

--- tp1-escaner-elemental/scanner.c
+++ tp1-escaner-elemental/scanner.c
@@ -5,36 +5,35 @@
 #include "scanner.h"
 
 #define CANT_ESTADOS_ACEPTORES 4
-#define CANT_ESTADOS_CENTINELA 5
-#define CANT_ESTADOS_A_PARAR 5
+#define CANT_ESTADOS_CENTINELA 4
 #define CANT_COLUMNAS 7
-#define CANT_FILAS 10
+#define CANT_FILAS 5
 
 const int ESTADO_INICIAL = E0;
 
-const int ESTADOS_CENTINELA[CANT_ESTADOS_CENTINELA] = {E2, E4, E6 , E8, E9};
-const int ESTADOS_ACEPTORES[CANT_ESTADOS_ACEPTORES] = {E2, E4, E8, E9};
-const int ESTADOS_A_PARAR[CANT_ESTADOS_A_PARAR] = {E2, E4, E6, E8, E9};
+const int ESTADOS_CENTINELA[CANT_ESTADOS_CENTINELA] = {E5, E99, E129, E189};
+const int ESTADOS_ACEPTORES[CANT_ESTADOS_ACEPTORES] = {E5, E99, E129, E189};
 
 const int TT[CANT_FILAS][CANT_COLUMNAS] = {
-/* T.T     L    0    D    #    seq. escape  Otro  fdt*/
-/* E0- */ {E1,  E5,  E3,  E7,  E0,          E5,   E9},
-/* E1  */ {E1,  E1,  E1,  E2,  E2,          E2,   E2},
-/* E2+ */ {E99, E99, E99, E99, E99,         E99,  E99},
-/* E3  */ {E4,  E3,  E3,  E4,  E4,          E4,   E4},
-/* E4+ */ {E99, E99, E99, E99, E99,         E99,  E99},
-/* E5  */ {E6,  E6,  E6,  E6,  E6,          E5,   E6},
-/* E6  */ {E99, E99, E99, E99, E99,         E99,  E99},
-/* E7  */ {E8,  E8,  E8,  E8,  E8,          E8,   E8},
-/* E8+ */ {E99, E99, E99, E99, E99,         E99,  E99},
-/* E9+ */ {E99, E99, E99, E99, E99,         E99,  E99}
+/* T.T     L     0     D     #     seq. escape  Otro  fdt*/
+/* E0- */ {E1,   E3,   E2,   E4,   E0,          E3,   E5},
+/* E1  */ {E1,   E1,   E1,   E99,  E99,         E99,  E99},
+/* E2  */ {E129, E2,   E2,   E129, E129,        E129, E129},
+/* E3  */ {E159, E159, E159, E159, E159,        E3,   E159},
+/* E4  */ {E189, E189, E189, E189, E189,        E189, E189}
 };
+
+/*
+* Para la posteridad:
+* En caso de agregar algún otro lexema a reconocer más, no olvidar de modificar el switch que evalúa el estado.
+* Modificar defines de CANT_FILAS, CANT_ESTADOS_ACEPTORES (en caso que amerite), CANT_ESTADOS_CENTINELA (en caso que amerite).
+*/
 
 int scanFlujo(FILE* archivo, int lexemasEncontrados[]){
   int estado = ESTADO_INICIAL;
   char *caracterLeido = malloc(sizeof(char));
 
-  while(!pararDeAnalizar(estado)){
+  while(estado < E5){
     *caracterLeido = getc(archivo);
     int simbolo = categorizarCaracter(caracterLeido);
     estado = TT[estado][simbolo];
@@ -45,16 +44,16 @@ int scanFlujo(FILE* archivo, int lexemasEncontrados[]){
       ungetc(*caracterLeido, archivo);
       free(caracterLeido);
       switch(estado){
-      case E2:
+      case E99:
         lexemasEncontrados[tokenIdentificador] += 1;
         return tokenIdentificador;
-      case E4:
+      case E129:
         lexemasEncontrados[tokenConstanteEntera] += 1;
         return tokenConstanteEntera;
-      case E8:
+      case E189:
         lexemasEncontrados[tokenNumeral] += 1;
         return tokenNumeral;
-      case E9:
+      case E5:
         return tokenFDT;
       }
     }
@@ -91,10 +90,6 @@ int esAceptor(int estado){
 
 int esCentinela(int estado){
   return esUnoDe(estado, CANT_ESTADOS_CENTINELA, ESTADOS_CENTINELA);
-}
-
-int pararDeAnalizar(int estado){
-  return esUnoDe(estado, CANT_ESTADOS_A_PARAR, ESTADOS_A_PARAR);
 }
 
 int esUnoDe(int estado, const int CANTIDAD, const int ESTADOS_QUE_CUMPLEN[]){
--- tp1-escaner-elemental/scanner.h
+++ tp1-escaner-elemental/scanner.h
@@ -2,13 +2,12 @@
 #define SCANNER_H_INCLUDED
 
 enum TOKENS {tokenIdentificador, tokenConstanteEntera, tokenNumeral, tokenError, tokenFDT};
-enum ESTADOS {E0, E1, E2, E3, E4, E5, E6, E7, E8, E9, E99};
+enum ESTADOS {E0, E1, E2, E3, E4, E5, E99 = 99, E129 = 129, E159 = 159, E189 = 189};
 enum COLUMNAS {COL0, COL1, COL2, COL3, COL4, COL5, COL6};
 
 int esUnoDe(int estado, const int CANTIDAD, const int ESTADOS_QUE_CUMPLEN[]);
 int scanFlujo(FILE* archivo, int lexemasEncontrados[]);
 int categorizarCaracter(char *c);
-int pararDeAnalizar(int estado);
 int esCentinela(int estado);
 int esAceptor(int estado);
 
