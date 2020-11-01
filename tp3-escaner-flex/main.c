478bceaae42ce990001c3415e81a65d938e07337
Nahuel Farias <nahuelfarias812@gmail.com>
2020-11-01 17:27:16 -0300

    Primera version semifuncional

--- tp3-escaner-flex/main.c
+++ tp3-escaner-flex/main.c
@@ -12,9 +12,77 @@
 int main()
 {
 
+	TOKEN token;
+
+    do {
+		token = yylex();
+		switch (token) {
+            case tokenLeer:
+                puts("Token: Leer");
+            break;
+            case tokenDeclarar:
+                puts("Token: Declarar");
+            break;
+            case tokenEscribir:
+                puts("Token: Escribir");
+            break;
+            case tokenPrograma:
+                puts("Token: Programa");
+            break;
+            case tokenFinProg:
+                puts("Token: Fin-Prog");
+            break;
+            case tokenIdentificador:
+                puts("Token: Identificador\tlexema: ");
+                puts(yytext);
+            break;
+            case tokenConstanteEntera:
+                puts("Token: Constante\tlexema: ");
+                puts(yytext);
+            break;
+            case tokenAsig:
+                puts("Token: Asignación");
+            break;
+            case '+':
+                puts("Token: \'+\'");
+            break;
+            case '-':
+                puts("Token: \'-\'");
+            break;
+			case '*':
+                puts("Token: \'*\'");
+            break;
+			case '/':
+                puts("Token: \'/\'");
+            break;
+			case '(':
+                puts("Token: \'(\'");
+            break;
+			case ')':
+                puts("Token: \')\'");
+            break;
+			case ',':
+                puts("Token: \',\'");
+            break;
+			case ';':
+                puts("Token: \';\'");
+            break;
+            case tokenErrorComun:
+                puts("Error léxico: cadena desconocida: ");
+                puts(yytext);
+            break;
+            case tokenErrorIdentificador:
+                puts("Error léxico: identificador inválido: ");
+                puts(yytext);
+            break;
+            case tokenErrorConstante:
+                puts("Error léxico: constante inválida: ");
+                puts(yytext);
+            break;
+		}
+	} while (token != FDT);
+    
     printf("holis\n");
-    
-    
     return EXIT_SUCCESS;
     
 }
--- tp3-escaner-flex/tokens.h
+++ tp3-escaner-flex/tokens.h
@@ -1,2 +1,8 @@
-enum token {FDT, };
+enum token {FDT,
+            tokenLeer, tokenDeclarar, tokenEscribir, tokenPrograma, tokenFinProg, //Palabras reservadas
+            tokenIdentificador,
+            tokenConstanteEntera,
+            tokenAsig, // Asignacion
+            tokenErrorComun, tokenErrorIdentificador, tokenErrorConstante,
+            };
 typedef enum token TOKEN;
